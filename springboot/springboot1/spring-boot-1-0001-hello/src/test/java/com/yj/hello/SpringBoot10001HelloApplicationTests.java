306d46d43184f32f076e6ed86c29c38eac3bd9b8
muyi-jian <2628168756@qq.com>
2023-11-15 10:57:52 +0800

    springboot不同版本示例提交

---
+++ springboot/springboot1/spring-boot-1-0001-hello/src/test/java/com/yj/hello/SpringBoot10001HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yj.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot10001HelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ springboot/springboot2/spring-boot-2-0001-hello/src/test/java/com/yj/hello/SpringBoot20001HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yj.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot20001HelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
