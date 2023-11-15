e4a49b7a35fbda3f165c5dbe7d74ff101f173171
muyi-jian <2628168756@qq.com>
2023-11-15 11:03:28 +0800

    springboot不同版本示例提交

--- springboot/springboot1/spring-boot-1-0001-hello/src/test/java/com/yj/hello/SpringBoot10001HelloApplicationTests.java
+++ springboot/springboot1/spring-boot-1-0001-hello/src/test/java/com/yj/hello/SpringBoot10001HelloApplicationTests.java
@@ -1,7 +1,7 @@
 package com.yj.hello;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.annotations.Test;
 
 @SpringBootTest
 class SpringBoot10001HelloApplicationTests {
