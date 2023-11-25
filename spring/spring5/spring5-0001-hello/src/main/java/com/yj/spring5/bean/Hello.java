43207a007ad917997898fcf1273f223d55290783
muyi-jian <2628168756@qq.com>
2023-11-25 20:59:12 +0800

    spring5的hello示例

---
+++ spring/spring5/spring5-0001-hello/src/main/java/com/yj/spring5/bean/Hello.java
@@ -0,0 +1,12 @@
+package com.yj.spring5.bean;
+
+/**
+ * @author YangJian
+ * @date 2023/11/25 20:38
+ * @description
+ */
+public class Hello {
+    public void sayHello() {
+        System.out.println("Hello Spring5.x !!!");
+    }
+}
